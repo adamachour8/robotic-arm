82ccc9c707f4b9a78dfbfcbb79906944c05c2433
Adam <adamachour8@gmail.com>
2026-08-16 02:33:50 -0400

    firmware: servo calibration sketch + PCA9685 setup

---
+++ firmware/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver();
+
+void setup()
+{
+    Serial.begin(115200);
+    pca.begin();
+    pca.setPWMFreq(50);
+}
+
+void loop()
+{
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        int off = input.toInt();
+        if (off > 0)
+        {
+            pca.setPWM(1, 0, off);
+            Serial.println(off);
+        }
+    }
+}
